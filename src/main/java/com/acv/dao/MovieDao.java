29ffdbaa4e7e381ba6ed78c6bc8b298d1ca0cfce
Hee-jin506 <gmlwls35220@gmail.com>
2020-11-24 19:05:02 +0900

    remove all

--- src/main/java/com/acv/dao/MovieDao.java
+++
@@ -1,9 +0,0 @@
-package com.acv.dao;
-
-import java.util.List;
-import com.acv.domain.Movie;
-
-public interface MovieDao {
-  List<Movie> findAll(String keyword) throws Exception;
-  int insert(Movie movie) throws Exception;
-}
--- src/main/java/com/acv/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.acv.service;
-
-import java.util.List;
-import com.acv.domain.Movie;
-
-public interface MovieService {
-  int add(Movie movie) throws Exception;
-  List<Movie> list() throws Exception;
-}
