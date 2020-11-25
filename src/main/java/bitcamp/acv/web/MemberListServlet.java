4d6f522bbb874c180bb465a20ff8f315f6d8ab1a
이건목 <antoniolee@igeonmog-ui-MacBookPro.local>
2020-11-25 14:44:45 +0900

    docs : MemberListServlet

--- src/main/java/bitcamp/acv/web/MemberListServlet.java
+++ src/main/java/bitcamp/acv/web/MemberListServlet.java
@@ -86,7 +86,7 @@ public class MemberListServlet extends HttpServlet {
             + "<td>%s</td>" // email
             + "<td>%s</td>" // pw
             + "<td>%s</td>" // nick
-            + "<td>%s</td>" // photo
+            + "<td><img src=\"%s\" alt=\"사진추가\" width=\"120\" /></td>" // photo
             + "<td>%s</td>" // intro
             + "<td>%d</td>" // qno
             + "<td>%s</td>" // qan
