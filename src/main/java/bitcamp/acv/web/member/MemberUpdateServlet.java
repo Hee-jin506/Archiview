a932495b1bfea46e2a1bde8b928a7f63a31edee7
ryu seunghee <ryuseunghee.dev@gmail.com>
2020-11-30 20:43:15 +0900

    docs : charEncoding

--- src/main/java/bitcamp/acv/web/member/MemberUpdateServlet.java
+++ src/main/java/bitcamp/acv/web/member/MemberUpdateServlet.java
@@ -20,7 +20,7 @@ public class MemberUpdateServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    request.setCharacterEncoding("UTF-8");
+    //    request.setCharacterEncoding("UTF-8");
 
     ServletContext ctx = request.getServletContext();
     MemberService memberService =
