c00e9d41021ee9c3659b3b213769634d59eedca3
cgkim449 <cgkim449@gmail.com>
2020-12-02 02:33:31 +0900

    docs : TagListServlet.java

--- src/main/java/bitcamp/acv/web/tag/TagListServlet.java
+++ src/main/java/bitcamp/acv/web/tag/TagListServlet.java
@@ -3,6 +3,7 @@ package bitcamp.acv.web.tag;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -50,13 +51,55 @@ public class TagListServlet extends HttpServlet {
           + "</a>");
 
       out.println("<h1>[태그 목록]</h1>");
-      out.println("<form>" + 
-          "<input type='search' value='검색' name='keyword'>" + 
-          "</form>");
 
-      List<Tag> list = tagService.list();
-      out.printf("<span>총 태그 수 : %d</span>", list.size());
+      List<Tag> list = null;
 
+      String keyword = request.getParameter("keyword");
+      String keywordNumber = request.getParameter("keywordNumber");
+      String keywordTitle = request.getParameter("keywordTitle");
+      String keywordStatus = request.getParameter("keywordStatus");
+
+      if (keyword != null) {
+        list = tagService.list(keyword);
+
+      } else if (keywordNumber != null) {
+        HashMap<String,Object> keywordMap = new HashMap<>();
+        keywordMap.put("number", keywordNumber);
+        keywordMap.put("title", keywordTitle);
+        keywordMap.put("status", keywordStatus);
+
+        list = tagService.list(keywordMap);
+
+      } else {
+        list = tagService.list();
+      }
+
+      out.println("<p>");
+      out.println("<form action='list' method='get'>");
+      out.printf("<input type='text' name='keyword' value='%s'>\n",
+          keyword != null ? keyword : "");
+      out.println("<button>검색</button>");
+      out.println("</form>");
+      out.println("</p>");
+
+      out.println("<hr>");
+      out.println("<h2>상세 검색</h2>");
+      out.println("<p>");
+      out.println("<form action='list' method='get'>");
+      out.printf("태그번호: <input type='text' name='keywordNumber' value='%s'><br>\n",
+          keywordNumber != null ? keywordNumber : "");
+      out.printf("태그명: <input type='text' name='keywordTitle' value='%s'><br>\n",
+          keywordTitle != null ? keywordTitle : "");
+      out.printf("상태: <input type='text' name='keywordStatus' value='%s'><br>\n",
+          keywordStatus != null ? keywordStatus : "");
+      out.println("<button>검색</button>");
+      out.println("</form>");
+      out.println("</p>");
+
+      List<Tag> list2 = tagService.list();
+      out.printf("<span>총 태그 수 : %d</span>", list2.size());
+
+      out.println("<form action='multipleDelete' method='get'>");
       out.println("<table border=\"1\">");
       out.println("<tr>");
       out.println("<th>선택</th>");
@@ -66,12 +109,13 @@ public class TagListServlet extends HttpServlet {
       out.println("<th>등록일</th>");
       out.println("<th>상태</th>");
       out.println("</tr>");
+
       for (Tag tag : list ) {
 
         out.println("<tr>");
         out.printf(""
-            + "<td><input type='checkbox' name='tags' </td>\n" // 선택
-            + "<td>%d</td>" // no
+            + "<td><input type='checkbox' name='tags' value='%d'</td>\n" // 선택
+            + "<td>%1$d</td>" // no
             + "<td><a href='detail?no=%1$d'>%s</a></td>" // title
             + "<td>1</td>" //
             + "<td>%s</td>" // rdt
@@ -84,6 +128,8 @@ public class TagListServlet extends HttpServlet {
         out.println("</tr>");
       }
       out.println("</table>");
+      out.println("<button>[삭제]</button>");
+      out.println("</form>");
 
     } catch (Exception e) {
       out.printf("작업 처리 중 오류 발생! - %s\n", e.getMessage());
---
+++ src/main/java/bitcamp/acv/web/tag/TagMultipleDeleteServlet.java
@@ -0,0 +1,76 @@
+package bitcamp.acv.web.tag;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bitcamp.acv.service.TagService;
+
+
+@WebServlet("/tag/multipleDelete")
+public class TagMultipleDeleteServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    ServletContext ctx = request.getServletContext();
+    TagService tagService =
+        (TagService) ctx.getAttribute("tagService");
+
+    String[] tagNoList = request.getParameterValues("tags");
+
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<meta http-equiv='Refresh' content='1;list'>");
+    out.println("<title>태그 선택 삭제</title></head>");
+    out.println("<body>");
+    try {
+      out.println("<h1>태그 선택 삭제</h1>");
+
+      try {
+        int count = 0;
+
+        if (tagNoList != null) {
+          for (String tagNo : tagNoList) {
+            count += tagService.delete(Integer.parseInt(tagNo));
+          }
+        }
+
+        if (count == 0) {
+          out.printf("<p>해당 태그가 존재하지 않습니다.</p>\n");
+        } else {
+          out.printf("<p>태그를 삭제하였습니다.</p>\n");
+        }
+      } catch (Exception e) {
+        out.println("태그 삭제 중 오류 발생!");
+        e.printStackTrace();
+      }
+
+    } catch (Exception e) {
+      out.printf("<h2>작업 처리 중 오류 발생!</h2>");
+      out.printf("<pre>%s</pre>\n", e.getMessage());
+
+      StringWriter errOut = new StringWriter();
+      e.printStackTrace(new PrintWriter(errOut));
+
+      out.printf("<h3>상세 오류 내용</h3>");
+      out.printf("<pre>%s</pre>\n", errOut.toString());
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+
+}
