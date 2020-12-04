83ecf85beaa42c0dbab01e27acafbd0f41f3b753
Hee-jin506 <gmlwls35220@gmail.com>
2020-12-04 18:00:24 +0900

    docs: login.jsp

--- src/main/java/bitcamp/acv/web/main/TopbarServlet.java
+++ src/main/java/bitcamp/acv/web/main/TopbarServlet.java
@@ -1,7 +1,6 @@
 package bitcamp.acv.web.main;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +16,6 @@ public class TopbarServlet extends HttpServlet {
       throws ServletException, IOException {
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
 
     request.getRequestDispatcher("topbar.jsp");
   }
