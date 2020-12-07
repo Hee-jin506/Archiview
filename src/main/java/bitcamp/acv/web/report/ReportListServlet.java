57f9fdd145329fa98bb8158d735c42a1a34d9709
j-jieun <jsje02@gmail.com>
2020-12-07 09:47:37 +0900

    reportlist.jsp

--- src/main/java/bitcamp/acv/web/report/ReportListServlet.java
+++ src/main/java/bitcamp/acv/web/report/ReportListServlet.java
@@ -1,8 +1,6 @@
 package bitcamp.acv.web.report;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -25,17 +23,8 @@ public class ReportListServlet extends HttpServlet {
     ReportService reportService = (ReportService) ctx.getAttribute("reportService");
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>[신고 관리]</title></head>");
-    out.println("<body>");
 
     try {
-      out.println("<h1>[신고 관리]</h1>");
 
       List<Report> list = null;
 
@@ -48,73 +37,12 @@ public class ReportListServlet extends HttpServlet {
         list = reportService.list();
       }
 
-      out.println("<table border='1'>");
-      out.println("<thead><tr>"
-          + "<th>신고번호</th>"
-          + "<th>신고유형</th>"
-          + "<th>신고대상</th>"
-          + "<th>신고자</th>"
-          + "<th>내용</th>"
-          + "<th>신고일</th>"
-          + "<th>상태</th>"
-          + "</tr></thead>");
-
-      out.println("<tbody>");
-
-      for (Report report : list) {
-        String type = null;
-        switch (report.getReportedType()) {
-          case 1 :
-            type = "회원";
-            break;
-          case 2:
-            type = "게시물";
-            break;
-          case 3:
-            type = "댓글";
-            break;
-          default:
-            type = "태그";
-        }
-
-        out.printf("<tr>"
-            + "<td>%d</td>\n"
-            + "<td><a href='detail?no=%1$d'>%s</a></td>\n"
-            + "<td>%d</td>\n"
-            + "<td>%d</td>\n"
-            + "<td>%s</td>\n"
-            + "<td>%s</td>\n"
-            + "<td>%s</td>\n"
-            + "</tr>\n",
-            report.getNo(), // 신고 번호
-            type, // 신고 유형
-            report.getReportedNo(), // 신고된 대상 번호
-            report.getReportingMember().getNo(),  // 신고한 회원
-            report.getProcessingContent(), // 신고 처리 내용
-            report.getReportedDate(), // 신고 일시
-            report.getStatus()); // 신고 처리 상태
-      }
-
-      out.println("</tbody>");
-      out.println("</table>");
-
-      out.println("<p>");
-      out.println("<form action='list' method='get'>");
-      out.printf("검색어: <input type='text' name='keyword' value='%s'>\n",
-          keyword != null ? keyword : "");
-      out.println("<button>검색</button>");
-      out.println("</form>");
-      out.println("</p>");
+      request.setAttribute("list", list);
+      request.getRequestDispatcher("/report/list.jsp").include(request, response);
 
     } catch (Exception e) {
-      out.println("<h2>작업 처리 중 오류 발생!</h2>");
-      out.printf("<pre>%s</pre>\n", e.getMessage());
-
-      StringWriter errOut = new StringWriter();
-      e.printStackTrace(new PrintWriter(errOut));
-      out.println("<h3>상세 오류 내용</h3>");
-      out.printf("<pre>%s</pre>\n", errOut.toString());
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error.jsp").forward(request, response);
     }
   }
-
 }
---
+++ src/main/webapp/report/list.jsp
@@ -0,0 +1,77 @@
+<%@page import="bitcamp.acv.domain.Report"%>
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%!
+    private String getType(int reportedType) {
+      switch (reportedType) {
+        case 1 :
+          return "회원";
+        case 2:
+          return "게시물";
+        case 3:
+          return "댓글";
+        default:
+          return "태그";
+      }
+    }
+    %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>[신고 관리]</title></head>
+<body>
+
+<h1>[신고 관리] - JSP</h1>
+
+<table border='1'>
+<thead>
+<tr>
+  <th>신고번호</th>
+  <th>신고유형</th>
+  <th>신고대상</th>
+  <th>신고자</th>
+  <th>내용</th>
+  <th>신고일</th>
+  <th>상태</th>
+</tr>
+</thead>
+
+<tbody>
+<%
+Exception ex = (Exception) request.getAttribute("exception");
+if (ex != null) {%>
+<tr>
+<td colspan="6">작업 목록을 가져오는 중 오류 발생!</td>
+</tr>
+<%} else {
+  List<Report> list = (List<Report>) request.getAttribute("list");
+  for (Report report : list) {
+  %>
+<tr>
+  <td><%=report.getNo()%></td>
+  <td><a href='detail?no=<%=report.getNo()%>'><%=getType(report.getReportedType())%></a></td>
+  <td><%=report.getReportedNo()%></td>
+  <td><%=report.getReportingMember().getNo()%></td>
+  <td><%=report.getProcessingContent()%></td>
+  <td><%=report.getReportedDate() %></td>
+  <td><%=report.getStatus()%></td>
+</tr>
+<%}  
+}%>
+
+</tbody>
+</table>
+<p>
+<%
+String keyword = request.getParameter("keyword");
+%>
+
+<form action='list' method='get'>
+검색어: <input type='text' name='keyword' value='<%=keyword != null ? keyword : ""%>'>
+<button>검색</button>
+</form>
+</p>
+</body>
+</html>
