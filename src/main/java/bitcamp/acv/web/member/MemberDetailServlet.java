92a1ae79d07b9b5df60497c94f63f5b6e873555a
ryu seunghee <ryuseunghee.dev@gmail.com>
2020-12-11 19:11:18 +0900

    docs member jsp

--- src/main/java/bitcamp/acv/web/member/MemberDetailServlet.java
+++ src/main/java/bitcamp/acv/web/member/MemberDetailServlet.java
@@ -1,8 +1,6 @@
 package bitcamp.acv.web.member;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -27,71 +25,21 @@ public class MemberDetailServlet  extends HttpServlet {
         (MemberService) ctx.getAttribute("memberService");
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head><title>회원상세조회</title></head>");
-    out.println("<body>");
 
     try {
-      out.println("<h1>[회원 상세 조회]</h1>");
       int no = Integer.parseInt(request.getParameter("no"));
       Member member = memberService.get(no);
 
       if (member == null) {
-        out.println("<p>해당 회원이 없습니다.</p>");
-      } else {
-        out.println("<form action='updatePhoto' method='post' enctype='multipart/form-data'>");
-        out.printf("<input type='hidden' name='no' value='%d'><br>\n",
-            member.getNo());
-        out.printf("<a href='../upload/%s'>\n<img src='../upload/%1$s_150x150.jpg'></a><br>\n",
-            member.getPhoto());
-        out.println("<input type='file' name='photo'>");
-        out.println("<button>변경</button>");
-        out.println("</form>");
-        out.println("<br>");
-
-        out.println("<form action='update' method='post'>");
-        out.printf("<input type='hidden' name='no' value='%d'><br>\n",
-            member.getNo());
-        out.printf("번호 - %s<br>\n", member.getNo());
-        out.printf("이름 - %s<br>\n", member.getName());
-        out.printf("이메일: %s<br>\n",
-            member.getEmail());
-        out.printf("암호 - %s<br>\n", member.getPassword());
-        out.printf("사진 - %s<br>\n", member.getPhoto());
-        out.printf("닉네임: <input type='text' name='nickName' value='%s'><br>\n",
-            member.getNickName());
-        out.printf("소개: <textarea name='intro'>%s</textarea><br>\n",
-            member.getIntro());
-        out.printf("비밀번호 힌트 질문 번호 - %d<br>\n", member.getQuestionsNo());
-        out.printf("비밀번호 힌트 정답 - %s<br>\n", member.getQuestionsAnswer());
-        out.printf("회원 가입일 - %s<br>\n", member.getRegisteredDate());
-        out.printf("회원 상태 - %s<br>\n", member.getStatusTitle());
-        out.printf("회원 상태 변경일 - %s<br>\n", member.getStatusModifiedDate());
-        out.println("<button>변경</button>");
-
-        if (member.getStatus() == 1) {
-          out.printf("<a href='inactive?no=%d'>비활성화</a>\n", member.getNo());
-        } else if (member.getStatus() == 2) {
-          out.printf("<a href='active?no=%d'>활성화</a>\n", member.getNo());
-        } else if (member.getStatus() == 3) {
-          out.println("탈퇴");
-        }
-        out.println("</p>");
-        out.println("</form>");
-
+        throw new Exception("해당 회원이 없습니다.");
       }
+
+      request.setAttribute("member", member);
+      request.getRequestDispatcher("/member/detail.jsp").include(request, response);
+
     } catch (Exception e) {
-      out.printf("<p>작업 처리 중 오류 발생! - %s</p>\n", e.getMessage());
-
-      StringWriter errOut = new StringWriter();
-      e.printStackTrace(new PrintWriter(errOut));
-
-      out.printf("<pre>%s</pre>\n", errOut.toString());
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error.jsp").forward(request, response);
     }
-
-    out.println("</body></html>");
   }
 }
