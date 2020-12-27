ee62326bd18f545685e9dbbd5abe4afac159e55a
Gunmok-Lee <gunmok.l@gmail.com>
2020-12-27 18:37:43 +0900

    docs : option/profileUpdate

--- src/main/java/bitcamp/acv/web/option/OptionController.java
+++ src/main/java/bitcamp/acv/web/option/OptionController.java
@@ -1,13 +1,15 @@
 package bitcamp.acv.web.option;
 
+import java.io.File;
 import java.util.UUID;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import bitcamp.acv.domain.Member;
 import bitcamp.acv.service.MemberService;
@@ -23,6 +25,45 @@ public class OptionController {
   @Autowired MemberService memberService;
   @Autowired ServletContext servletContext;
 
+  @RequestMapping("profile")
+  protected ModelAndView profile(HttpServletRequest request) throws Exception {
+
+    ModelAndView mv = new ModelAndView();
+    HttpSession session = request.getSession();
+
+    Member member = (Member) session.getAttribute("loginUser");
+    if (member == null) {
+      throw new Exception("로그인 되어있지 않습니다.");
+    } else {
+      mv.addObject("member", member);
+      mv.setViewName("option/profile");
+      return mv;
+    }
+  }
+
+  @PostMapping("profileUpdate")
+  protected String update(
+      HttpSession session,
+      HttpServletRequest request,
+      MultipartFile photo,
+      String nickName,
+      String intro) throws Exception{
+    Member member = (Member) session.getAttribute("loginUser");
+
+    if (photo.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      String saveFilePath = servletContext.getRealPath("/upload/" + filename);
+      photo.transferTo(new File(saveFilePath));
+      member.setPhoto(filename);
+
+      // 회원 사진의 썸네일 이미지 파일 생성하기
+      generatePhotoThumbnail(saveFilePath);
+    }
+    member.setNickName(nickName);
+    member.setIntro(intro);
+    memberService.update(member);
+    return "redirect:profile";
+  }
   private void generatePhotoThumbnail(String saveFilePath) {
     try {
       Thumbnails.of(saveFilePath)
@@ -49,41 +90,6 @@ public class OptionController {
     } catch (Exception e) {
       e.printStackTrace();
     }
-  }
-
-
-  @RequestMapping("profile")
-  protected ModelAndView profile(HttpServletRequest request) throws Exception {
-
-    ModelAndView mv = new ModelAndView();
-    HttpSession session = request.getSession();
-
-    Member member = (Member) session.getAttribute("loginUser");
-    if (member == null) {
-      throw new Exception("로그인 되어있지 않습니다.");
-    } else {
-      mv.addObject("member", member);
-      mv.setViewName("option/profile");
-      return mv;
-    }
-  }
-
-  @RequestMapping("profileUpdate")
-  protected String update(HttpServletRequest request, HttpServletRequest response, Member member) throws Exception {
-    member.setNo(Integer.parseInt(request.getParameter("no")));
-
-    Part photoPart = request.getPart("photo");
-    if (photoPart.getSize() > 0) {
-      String filename = UUID.randomUUID().toString();
-      String saveFilePath = servletContext.getRealPath("/upload/" + filename);
-      photoPart.write(saveFilePath);
-      member.setPhoto(filename);
-
-      // 회원 사진의 썸네일 이미지 파일 생성하기
-      generatePhotoThumbnail(saveFilePath);
-    }
-    memberService.update(member);
-    return "redirect:profile";
   }
 
   @RequestMapping("passwordHint")
